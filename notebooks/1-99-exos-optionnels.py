6a7b2770acb4e9fad62a3a66f57ab20d15f5b334
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-07-27 16:07:51 +0200

    optional Python exercises

---
+++ notebooks/1-99-exos-optionnels.py
@@ -0,0 +1,51 @@
+# %% [markdown]
+# Licence CC BY-NC-ND, Valérie Roy & Thierry Parmentelat
+
+# %%
+from IPython.display import HTML
+HTML(url="https://raw.githubusercontent.com/ue12-p22/python-numerique/main/notebooks/_static/style.html")
+
+
+# %% [markdown]
+# # exercices optionnels
+
+# %%
+from IPython.display import HTML
+HTML(url="https://raw.githubusercontent.com/ue12-p22/python-numerique/main/notebooks/_static/style.html")
+
+
+# %% [markdown]
+# pour les rapides qui ont déjà terminé
+
+# %% [markdown]
+# à faire directement sur la plateforme codewars
+
+# %% [markdown]
+# ## facile (8 kyu)
+
+# %% [markdown]
+# * Cascading Subsets  
+#   https://www.codewars.com/kata/545af3d185166a3dec001190/train/python
+#
+# * What is between ?  
+#   https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/python
+#
+# * Beginner Series #4 Cockroach  
+#   https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/python
+
+# %% [markdown]
+# ## moyen (7 kyu)
+
+# %% [markdown]
+# * Integer Difference  
+#   https://www.codewars.com/kata/57741d8f10a0a66915000001/train/python
+
+# %% [markdown]
+# ## un peu plus dur (6 kyu)
+
+# %% [markdown]
+# * Bishop movement checker  
+#   https://www.codewars.com/kata/6135e4f40cffda0007ce356b/train/python
+
+# %% [markdown]
+# Comme vous le voyez sur codewars il y a des centaines de problèmes de programmation (et pas que en Python d'ailleurs), faites votre choix...
